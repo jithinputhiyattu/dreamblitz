53e8a26a1f42346e856f730d1bf23ce92e4e1800
Puthiyattu <jithinputhiyattu@gmail.com>
2019-04-06 10:58:12 +0530

    Change colours

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -11,19 +11,19 @@ import Triangle from '../components/Triangle';
 const Background = () => (
   <div>
     <Triangle
-      color="green"
+      color="backgroundDark"
       height={['35vh', '80vh']}
       width={['95vw', '60vw']}
     />
 
     <Triangle
-      color="orange"
+      color="secondary"
       height={['38vh', '80vh']}
       width={['50vw', '35vw']}
     />
 
     <Triangle
-      color="blue"
+      color="primaryDark"
       height={['25vh', '35vh']}
       width={['75vw', '60vw']}
       invertX
