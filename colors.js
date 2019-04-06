13f139e4f29b9a8c3b4c63a2e350c4e04558b0ca
Puthiyattu <jithinputhiyattu@gmail.com>
2019-04-06 11:19:11 +0530

    Change theame

--- colors.js
+++ colors.js
@@ -1,12 +1,25 @@
+// module.exports = {
+//   background: '#FFFFFF',
+//   backgroundDark: '#f0e6f6',
+
+//   primary: '#7c37ad',
+//   primaryLight: '#ae66df',
+//   primaryDark: '#4b007d',
+
+//   secondary: '#ff4081',
+//   secondaryLight: '#ff79b0',
+//   secondaryDark: '#c60055',
+// };
+
 module.exports = {
   background: '#FFFFFF',
-  backgroundDark: '#f0e6f6',
+  backgroundDark: '#fff9e6',
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary: '#660033',
+  primaryLight: '#000000',
+  primaryDark: '#800000',
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
+  secondary: '#ff4d4d',
+  secondaryLight: '#e0ebeb',
   secondaryDark: '#c60055',
 };
